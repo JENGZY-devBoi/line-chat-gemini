47a25057c177c51027dfb26cd8f62d3bb39c98ef
Teerapong Saisrikaew <teerapong.s@clicknext.com>
2024-11-26 11:07:32 +0700

    init

---
+++ main.py
@@ -0,0 +1,54 @@
+import uvicorn
+from fastapi import FastAPI, HTTPException, Request
+import os
+from dotenv import load_dotenv
+
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (Configuration,
+                                  ApiClient,
+                                  MessagingApi,
+                                  ReplyMessageRequest,
+                                  TextMessage)
+from linebot.v3.webhooks import (MessageEvent,TextMessageContent)
+
+load_dotenv()
+
+app = FastAPI()
+
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+CHANNEL_KEY = os.getenv("CHANNEL_KEY")
+
+configuration = Configuration(access_token=ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_KEY)
+
+@app.post("/message")
+async def message(request: Request):
+    signature = request.headers.get("X-Line-Signature")
+    if not signature:
+        raise HTTPException(status_code=400, detail="X-Line-Signature header is missing")
+    
+    body = await request.body()
+    
+    try:
+        handler.handle(body.decode("UTF-8"), signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Invalid signature")
+
+    return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event: MessageEvent):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
